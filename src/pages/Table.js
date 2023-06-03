be8eb8ba50442fc719da4a141dfcf9e15b450bf8
brunoangeloccimartins <brunoangelocci@gmail.com>
2023-06-03 14:54:32 -0300

    Requisito 2, completo

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -6,69 +6,75 @@ function Table() {
   const header = planetFilter[0];
 
   const [inputValue, setInputValue] = useState({});
-  const [filteredPlanets, setFilteredPlanets] = useState(planetFilter)
-  
+  const [filteredPlanets, setFilteredPlanets] = useState(planetFilter);
+
   const handleChange = (e) => {
     const { value, name } = e.target;
-    
+
     setInputValue((prevValues) => ({
-      ...prevValues, [name]:value
+      ...prevValues,
+      [name]: value,
     }));
   };
-  
+
   const handleFilter = () => {
-    const filteredData = planetFilter.filter((item) => {
-      console.log(item.name,inputValue)
-      if (inputValue['name-filter'].length !== 0 && !item.name.toLowerCase().includes(inputValue['name-filter'].toLowerCase())) {
-        return false;
-      }
-      return true
-    })
-    setFilteredPlanets(filteredData)
-    console.log(filteredData)
-  }
+    const input = inputValue['name-filter'];
+    const filteredData = planetFilter.filter((item) => !(input
+      && input.length !== 0
+      && !item.name.toLowerCase().includes(input.toLowerCase())));
+    setFilteredPlanets(filteredData);
+  };
 
   useEffect(() => {
     callApi();
   }, []);
 
   useEffect(() => {
-    if(inputValue['name-filter']?.length !== 0) {
+    if (inputValue['name-filter']?.length !== 0) {
       handleFilter();
     }
-  },[inputValue])
+  }, [inputValue]);
 
   if (header === undefined) {
-    return (<p>Loading</p>);
+    return <p>Loading</p>;
   }
+
+  const { 'name-filter': nameFilter = '' } = inputValue;
 
   return (
     <main>
       <input
         key="name-input"
         type="text"
-        name='name-filter'
+        name="name-filter"
         data-testid="name-filter"
-        value={ inputValue['name-filter'] || '' }
+        value={ nameFilter }
         onChange={ handleChange }
       />
       <table>
         <thead>
           <tr>
             {Object.keys(header).map((element) => (
-              <th key={ element }>
-                {element.toUpperCase()}
-              </th>))}
+              <th key={ element }>{element.toUpperCase()}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {filteredPlanets.map((planet) => (
-            <tr key={ planet.name }>
-              {Object.values(planet).map((value) => (
-                <td key={ value }>{value}</td>
-              ))}
-            </tr>
-          ))}
+          {(nameFilter === '' || nameFilter === undefined)
+            ? planetFilter.map((planet) => (
+              <tr key={ planet.name }>
+                {Object.values(planet).map((value) => (
+                  <td key={ value }>{value}</td>
+                ))}
+              </tr>
+            ))
+            : filteredPlanets.map((planet) => (
+              <tr key={ planet.name }>
+                {Object.values(planet).map((value) => (
+                  <td key={ value }>{value}</td>
+                ))}
+              </tr>
+            ))}
         </tbody>
       </table>
     </main>
